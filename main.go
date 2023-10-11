59bf7230ccd7113455d150f5f80e4b6633054b1b
runik <xammatovrunar891@gmail.com>
2023-10-11 21:21:59 +0300

    correction

--- main.go
+++ main.go
@@ -15,10 +15,6 @@ func romanToArabic(roman string) int {
 		'I': 1,
 		'V': 5,
 		'X': 10,
-		'L': 50,
-		'C': 100,
-		'D': 500,
-		'M': 1000,
 	}
 
 	result := 0
@@ -39,44 +35,80 @@ func romanToArabic(roman string) int {
 	return result
 }
 
+// Функция для преобразования арабских чисел в римские
+func arabicToRoman(arabic int) string {
+	if arabic < 1 || arabic > 3999 {
+		return "Недопустимое значение"
+	}
+
+	romanNumerals := []string{"X", "IX", "V", "IV", "I"}
+	arabicValues := []int{10, 9, 5, 4, 1}
+
+	roman := ""
+
+	for i := 0; i < len(arabicValues); i++ {
+		for arabic >= arabicValues[i] {
+			roman += romanNumerals[i]
+			arabic -= arabicValues[i]
+		}
+	}
+
+	return roman
+}
+
 func evaluateExpression(expression string) (interface{}, error) {
 	parts := strings.Split(expression, " ")
 	if len(parts) != 3 {
 		return nil, fmt.Errorf("Неправильный формат выражения. Используйте формат 'число оператор число'.")
 	}
 
-	firstOperand, err := strconv.Atoi(parts[0])
-	if err != nil {
-		// Если не удалось преобразовать в арабское число, попробуем римское
+	var firstOperand, secondOperand int
+	var err error
+
+	if firstOperand, err = strconv.Atoi(parts[0]); err != nil {
 		firstOperand = romanToArabic(parts[0])
 	}
 
 	operator := parts[1]
 
-	secondOperand, err := strconv.Atoi(parts[2])
-	if err != nil {
-		// Если не удалось преобразовать в арабское число, попробуем римское
+	if secondOperand, err = strconv.Atoi(parts[2]); err != nil {
 		secondOperand = romanToArabic(parts[2])
+	}
+
+	if (firstOperand < 1 || firstOperand > 10) || (secondOperand < 1 || secondOperand > 10) {
+		return nil, fmt.Errorf("Число меньше 1 и больше 10, введите другое число")
 	}
 
 	switch operator {
 	case "+":
 		result := firstOperand + secondOperand
+		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+			return arabicToRoman(result), nil
+		}
 		return result, nil
 	case "-":
 		result := firstOperand - secondOperand
 		if result <= 0 {
 			return nil, fmt.Errorf("Арабские числа не могут быть отрицательными или равными нулю")
 		}
+		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+			return arabicToRoman(result), nil
+		}
 		return result, nil
 	case "*":
 		result := firstOperand * secondOperand
+		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+			return arabicToRoman(result), nil
+		}
 		return result, nil
 	case "/":
 		if secondOperand == 0 {
 			return nil, fmt.Errorf("Деление на ноль невозможно")
 		}
 		result := firstOperand / secondOperand
+		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+			return arabicToRoman(result), nil
+		}
 		return result, nil
 	default:
 		return nil, fmt.Errorf("Недопустимый оператор. Используйте: +, -, *, /")
@@ -99,10 +131,14 @@ func main() {
 	result, err := evaluateExpression(expression)
 	if err != nil {
 		fmt.Printf("Ошибка: %v\n", err)
+		return
+	}
+
+	// Определяем, какой формат использовался в выражении и выводим
+	if num, ok := result.(int); ok {
+		fmt.Printf("Результат: %d\n", num)
 	} else {
-		// Если результат - арабское число, выводим как арабское
-		if num, ok := result.(int); ok {
-			fmt.Printf("Результат: %d\n", num)
-		}
+		romanResult := result.(string)
+		fmt.Printf("Результат: %s\n", romanResult)
 	}
 }
