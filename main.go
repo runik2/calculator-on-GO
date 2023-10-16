4621a1670234a92fea36510abaf61df37de5839c
runik <xammatovrunar891@gmail.com>
2023-10-16 12:45:18 +0300

    correction of arabic numbers

--- main.go
+++ main.go
@@ -9,12 +9,20 @@ import (
 	"strings"
 )
 
-// Функция для преобразования римских чисел в арабские
+func isValidRoman(roman string) bool {
+	romanPattern := regexp.MustCompile(`^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$`)
+	return romanPattern.MatchString(roman)
+}
+
 func romanToArabic(roman string) int {
 	romanNumerals := map[rune]int{
 		'I': 1,
 		'V': 5,
 		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
 
 	result := 0
@@ -35,21 +43,36 @@ func romanToArabic(roman string) int {
 	return result
 }
 
-// Функция для преобразования арабских чисел в римские
 func arabicToRoman(arabic int) string {
 	if arabic < 1 || arabic > 3999 {
 		return "Недопустимое значение"
 	}
 
-	romanNumerals := []string{"X", "IX", "V", "IV", "I"}
-	arabicValues := []int{10, 9, 5, 4, 1}
+	romanNumerals := []struct {
+		Value  int
+		Symbol string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
 
 	roman := ""
 
-	for i := 0; i < len(arabicValues); i++ {
-		for arabic >= arabicValues[i] {
-			roman += romanNumerals[i]
-			arabic -= arabicValues[i]
+	for _, numeral := range romanNumerals {
+		for arabic >= numeral.Value {
+			roman += numeral.Symbol
+			arabic -= numeral.Value
 		}
 	}
 
@@ -66,23 +89,28 @@ func evaluateExpression(expression string) (interface{}, error) {
 	var err error
 
 	if firstOperand, err = strconv.Atoi(parts[0]); err != nil {
+		if !isValidRoman(parts[0]) {
+			return nil, fmt.Errorf("Неправильный формат числа")
+		}
 		firstOperand = romanToArabic(parts[0])
 	}
 
 	operator := parts[1]
 
 	if secondOperand, err = strconv.Atoi(parts[2]); err != nil {
+		if !isValidRoman(parts[2]) {
+			return nil, fmt.Errorf("Неправильный формат числа")
+		}
 		secondOperand = romanToArabic(parts[2])
 	}
 
 	if (firstOperand < 1 || firstOperand > 10) || (secondOperand < 1 || secondOperand > 10) {
 		return nil, fmt.Errorf("Число меньше 1 и больше 10, введите другое число")
 	}
-
 	switch operator {
 	case "+":
 		result := firstOperand + secondOperand
-		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+		if isValidRoman(parts[0]) && isValidRoman(parts[2]) {
 			return arabicToRoman(result), nil
 		}
 		return result, nil
@@ -91,13 +119,13 @@ func evaluateExpression(expression string) (interface{}, error) {
 		if result <= 0 {
 			return nil, fmt.Errorf("Арабские числа не могут быть отрицательными или равными нулю")
 		}
-		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+		if isValidRoman(parts[0]) && isValidRoman(parts[2]) {
 			return arabicToRoman(result), nil
 		}
 		return result, nil
 	case "*":
 		result := firstOperand * secondOperand
-		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+		if isValidRoman(parts[0]) && isValidRoman(parts[2]) {
 			return arabicToRoman(result), nil
 		}
 		return result, nil
@@ -106,7 +134,7 @@ func evaluateExpression(expression string) (interface{}, error) {
 			return nil, fmt.Errorf("Деление на ноль невозможно")
 		}
 		result := firstOperand / secondOperand
-		if regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[0]) && regexp.MustCompile(`^[IVXLCDM]+$`).MatchString(parts[2]) {
+		if isValidRoman(parts[0]) && isValidRoman(parts[2]) {
 			return arabicToRoman(result), nil
 		}
 		return result, nil
@@ -122,7 +150,6 @@ func main() {
 	expression, _ := reader.ReadString('\n')
 	expression = strings.TrimSpace(expression)
 
-	// Проверяем, что введено корректное выражение
 	if !regexp.MustCompile(`^\d+\s[+\-*/]\s\d+$|^[IVXLCDM]+\s[+\-*/]\s[IVXLCDM]+$`).MatchString(expression) {
 		fmt.Println("Неправильный формат ввода.")
 		return
@@ -134,7 +161,6 @@ func main() {
 		return
 	}
 
-	// Определяем, какой формат использовался в выражении и выводим
 	if num, ok := result.(int); ok {
 		fmt.Printf("Результат: %d\n", num)
 	} else {
